3754a8c10dad7682580a50b8021900f978b9bf20
Justin Ethier <justin.ethier@gmail.com>
2014-07-20 14:35:55 -0400

    Initial file

---
+++ hashtbl.h
@@ -0,0 +1,23 @@
+#ifndef __HASHTBL_H__
+#define __HASHTBL_H__
+
+struct nlist { /* table entry: */
+    struct nlist *next; /* next entry in chain */
+    char *name; /* defined name */
+    char *defn; /* replacement text */
+};
+
+typedef struct nlist *hashtbl;
+
+struct nlist **htinit();
+void htfree(struct nlist **ht);
+
+/* hash: form hash value for string s */
+unsigned hash(char *s);
+
+/* lookup: look for s in hashtab */
+struct nlist *htlookup(struct nlist **hashtab, char *s);
+
+/* put: put (name, defn) in hashtab */
+struct nlist *htput(struct nlist **hashtab, char *name, char *defn);
+#endif
